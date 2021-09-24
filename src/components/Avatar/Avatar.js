e1db19fb74b47d069ed94d5478004ff60ae605bc
Dan <danielsalazar.cc@gmail.com>
2021-09-23 21:55:17 -0400

    Refactor avatar component

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { AvatarImg } from './avatarStyles'
 
-const URL_IMAGE = 'https://cdn.discordapp.com/avatars/846973614439137320/0b5c6e74fbc90c6a5c336ace7a634ee4.png?size=256'
-
-function Avatar ({ width, height }) {
+function Avatar ({ width, height, avatarImage }) {
     return (
-        <AvatarImg src={URL_IMAGE} width={width} height={height} />
+        <AvatarImg src={avatarImage} width={width} height={height} />
     )
 }
 
