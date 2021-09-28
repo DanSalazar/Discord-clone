756b182eec93e3ecef487f03f11de5fc5a319c7d
Dan <danielsalazar.cc@gmail.com>
2021-09-27 23:43:27 -0400

    feat(root): Add scroll to the last message in chat view

--- src/components/Chat/ChatBox.js
+++ src/components/Chat/ChatBox.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import ChatMessage from './ChatMessage'
 import { ChatBoxMessagesContent, ChatBoxWrapper } from './chatStyles'
 
 function ChatBox ({ messages }) {
+  const messagesWrapper = useRef()
+
+  // Messages component scroll to the last message
+  useEffect(() => {
+    messagesWrapper.current.scrollTop = messagesWrapper.current.scrollHeight
+  }, [messages])
+
     return (
-        <ChatBoxWrapper>
+        <ChatBoxWrapper ref={messagesWrapper} >
             {messages &&
-              <ChatBoxMessagesContent>
+              <ChatBoxMessagesContent tabindex='-1'>
                 {messages.map(msg => (
                   <ChatMessage 
                     key={msg.id} 
