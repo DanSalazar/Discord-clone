b7eff1cf2b0be35573cd07e6583200b7f910dd1b
Daniel Salazar <danielsalazar.cc@gmail.com>
2022-10-23 22:08:15 -0400

    Delete login/register pages

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Login from './Pages/Login/Login'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './Pages/home'
 import ChannelPage from './Pages/channel'
-import ProtectedRoute from './Pages/ProtectedRoute'
-import Register from './Pages/Register/Register'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <ProtectedRoute 
-          path='/home'
-          exact 
-          component={Home}/>
-        <ProtectedRoute
-          path='/home/:id'
-          component={ChannelPage} />
-        <ProtectedRoute
-          path='/login'
-          component={Login}
-          toHome={true} />
-        <ProtectedRoute
-          path='/register'
-          component={Register}
-          toHome={true} /> 
+        <Route path='/' exact component={Home} />
+        <Route path=':channelid' component={ChannelPage} />
       </Switch>
     </Router>
   )
